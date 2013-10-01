0d536cf6449071a824ca80fa4e2820a17a695444
Rohit Kalkur <rkalkur@mitre.org>
2013-10-01 17:36:24 -0400

    - Adding in the EventsSpec and PublicMethodsSpec - Updating bootstrap-slider code to pass unit tests - Refactoring public interface code of boostrap-slider - Adding in code to ensure proper deletion/event unbinding when destroy() is called on slider

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,8 @@ module.exports = function(grunt) {
     // Task configuration.
     uglify: {
       dist: {
-        src: 'js/bootstrap-slider.js',
-        dest: 'dist/bootstrap-slider.min.js'
+        src: '<%= pkg.main %>',
+        dest: '<%= pkg.gruntConfig.dist.js %>'
       }
     },
     jshint: {
@@ -35,23 +35,83 @@ module.exports = function(grunt) {
         src: 'Gruntfile.js'
       },
       js: {
-        src: '<%= uglify.dist.src %>'
+        src: '<%= pkg.main %>'
+      },
+      spec : {
+        src: '<%= pkg.gruntConfig.spec %>',
+        options : {
+          globals : {
+            console: false,
+            $: false,
+            _: false,
+            _V_: false,
+            afterEach: false,
+            beforeEach: false,
+            confirm: false,
+            context: false,
+            describe: false,
+            expect: false,
+            it: false,
+            jasmine: false,
+            JSHINT: false,
+            mostRecentAjaxRequest: false,
+            qq: false,
+            runs: false,
+            spyOn: false,
+            spyOnEvent: false,
+            waitsFor: false,
+            xdescribe: false
+          }
+        }
+      }
+    },
+    jasmine : {
+      src : '<%= pkg.main %>',
+      options : {
+        specs : '<%= pkg.gruntConfig.spec %>',
+        vendor : ['<%= pkg.gruntConfig.js.jquery %>'],
+        styles : ['<%= pkg.gruntConfig.css.boostrap %>', '<%= pkg.gruntConfig.css.slider %>'],
+        template : '<%= pkg.gruntConfig.tpl.SpecRunner %>'
+      }
+    },
+    template : {
+      'generate-index-page' : {
+        options : {
+          data : {
+            js : {
+              modernizr : '<%= pkg.gruntConfig.js.modernizr %>',
+              jquery : '<%= pkg.gruntConfig.js.jquery %>',
+              slider : '<%= pkg.main %>'
+            },
+            css : {
+              bootstrap : '<%= pkg.gruntConfig.css.bootstrap %>',
+              slider : '<%= pkg.gruntConfig.css.slider %>'
+            }
+          }
+        },
+        files : {
+          'index.html' : ['<%= pkg.gruntConfig.tpl.index %>']
+        }
       }
     },
     watch: {
       js : {
-        files: '<%= uglify.dist.src %>',
-        tasks: ['jshint:js']
+        files: '<%= pkg.main %>',
+        tasks: ['jshint:js', 'jasmine']
       },
       gruntfile : {
         files: '<%= jshint.gruntfile %>',
         tasks: ['jshint:gruntfile']
+      },
+      spec : {
+        files: '<%= jshint.spec %>',
+        tasks: ['jshint:spec']
       }
     },
     connect: {
       server: {
         options: {
-          port: 9001
+          port: "<%= pkg.gruntConfig.devPort %>"
         }
       }
     },
@@ -65,13 +125,15 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-template');
 
   // Default task.
-  grunt.registerTask('test', ['jshint']);
-  grunt.registerTask('development', ['jshint', 'connect', 'open:development', 'watch']);
-  grunt.registerTask('production', ['jshint', 'uglify']);
+  grunt.registerTask('test', ['jshint', 'jasmine']);
+  grunt.registerTask('development', ['test', 'template', 'connect', 'open:development', 'watch']);
+  grunt.registerTask('production', ['test', 'uglify']);
 
 };
