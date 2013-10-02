c50cf7db5641be41b9d11aebbabc0bb1a0d72838
Kyle Kemp <kyle@seiyria.com>
2013-10-02 17:49:42 -0500

    added element data attribute tests

--- spec/ElementDataAttributesSpec.js
+++ spec/ElementDataAttributesSpec.js
@@ -2,43 +2,68 @@ describe("Element Data Attributes Tests", function() {
   var slider;
 
   it("reads the 'data-slider-min' property and sets it on slider", function() {
+    slider = $("#minSlider").slider();
+    slider.slider('setValue', 1);
 
+    var sliderValue = slider.slider('getValue');
+    expect(sliderValue).toBe(5);
   });
 
   it("reads the 'data-slider-max' property and sets it on slider", function() {
+    slider = $("#maxSlider").slider();
+    slider.slider('setValue', 10);
 
+    var sliderValue = slider.slider('getValue');
+    expect(sliderValue).toBe(5);
   });
 
   it("reads the 'data-slider-step' property and sets it on slider", function() {
 
+    slider = $("#stepSlider").slider();
+    //TODO How do you test this? Maybe manually trigger a slideChange event?
+    expect(true).toBeTruthy();
   });
 
   it("reads the 'data-slider-orientation' property and sets it on slider", function() {
-
+    slider = $("#orientationSlider").slider();
+      
+    var orientationClassApplied = $("#orientationSlider").parent("div.slider").hasClass("slider-vertical");
+    expect(orientationClassApplied).toBeTruthy();
   });
 
   it("reads the 'data-slider-value' property and sets it on slider", function() {
+    slider = $("#valueSlider").slider();
 
+    var sliderValue = slider.slider('getValue');
+    expect(sliderValue).toBe(5);
   });
 
   it("reads the 'data-slider-selection' property and sets it on slider", function() {
+    slider = $("#selectionSlider").slider();
+    slider.slider('setValue', 10);
 
+    var sliderSelectionWidthAtMaxValue = $("#selectionSlider").siblings("div.slider-track").children("div.slider-selection").width();
+    expect(sliderSelectionWidthAtMaxValue).toBe(0);
   });
 
   it("reads the 'data-slider-tooltip' property and sets it on slider", function() {
-
+    slider = $("#tooltipSlider").slider();
+    var tooltipIsHidden = $("#tooltipSlider").siblings("div.tooltip").hasClass("hide");
+    expect(tooltipIsHidden).toBeTruthy();
   });
 
   it("reads the 'data-slider-handle' property and sets it on slider", function() {
-
+    slider = $("#handleSlider").slider();
+    var handleIsSetToTriangle = $("#handleSlider").siblings("div.slider-track").children("div.slider-handle").hasClass("triangle");
+    expect(handleIsSetToTriangle).toBeTruthy();
   });
 
   it("reads the 'data-slider-reversed' property and sets it on slider", function() {
-
-  });
-
-  it("reads the 'data-slider-formatter' property and sets it on slider", function() {
-
+    slider = $("#reversedSlider").slider();
+    slider.slider('setValue', 10);
+      
+    var sliderSelectionHeightAtMaxValue = $("#reversedSlider").siblings("div.slider-track").children("div.slider-selection").width();
+    expect(sliderSelectionHeightAtMaxValue).toBe(0);
   });
 
   afterEach(function() {
--- tpl/SpecRunner.tpl
+++ tpl/SpecRunner.tpl
@@ -16,5 +16,16 @@
 	<!-- Slider used for PublicMethodsSpec and EventsSpec -->
 	<input id="testSlider1" type="text"/>
 	<input id="testSlider2" type="text"/>
+
+	<!-- Sliders used for ElementDataSttributesSpec -->
+	<input id="minSlider" type="text" data-slider-min="5"/>
+	<input id="maxSlider" type="text" data-slider-max="5"/>
+	<input id="orientationSlider" type="text" data-slider-orientation="vertical"/>
+	<input id="stepSlider" type="text" data-slider-step="5"/>
+	<input id="valueSlider" type="text" data-slider-value="5"/>
+	<input id="selectionSlider" type="text" data-slider-selection="after"/>
+	<input id="tooltipSlider" type="text" data-slider-tooltip="hide"/>
+	<input id="handleSlider" type="text" data-slider-handle="triangle"/>
+	<input id="reversedSlider" type="text" data-slider-reversed="true"/>
 </body>
 </html>
