755c2874780bde8a4abe2e51d1e2e521a675cf0a
pmoncunill <pmoncunill@hotmail.com>
2014-10-08 10:08:43 +0200

    Created test for zero value on slide

--- test/specs/ConflictingOptionsSpec.js
+++ test/specs/ConflictingOptionsSpec.js
@@ -10,6 +10,25 @@
 	This will help ensure that they are accounted for and do not arise again.
 */	
 describe("Conflicting Options Tests", function() {
+	
+	it("Should have the value zero when it is slided to zero", function() {
+		var testSlider = $("#testSlider1").slider({
+			value: 0,
+			step: 1
+		});
+		var flag = false;
+		var mouse = document.createEvent('MouseEvents');
+
+		testSlider.on('slide', function(slideEvt) {
+			expect(slideEvt.value).toBe(0);
+			flag = true;
+		});
+
+	      	testSlider.data('slider')._mousemove(mouse);
+		expect(flag).toBeTruthy();
+	
+    	});
+    
 	it("should set the `precision` to be the number of digits after the decimal of the `step` (assuming no `precision` is specified)", function() {
 		// Create Slider
 		var $testSlider = $("#testSlider1").slider({
