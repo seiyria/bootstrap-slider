b1ee114d50795bb26078f4fb54c407d51aa7ee66
Dominik Krebs <dominik.krebs@netzkollektiv.com>
2014-12-30 11:11:48 +0100

    js-accessible value was not changed by slider function, accessing the input's value was not possible (.val() / .value)

--- js/bootstrap-slider.js
+++ js/bootstrap-slider.js
@@ -1098,6 +1098,7 @@
 				var value = "value: '" + val + "'";
 				this.element.setAttribute('data', value);
 				this.element.setAttribute('value', val);
+                this.element.value = val;
 			},
 			_trigger: function(evt, val) {
 				val = (val || val === 0) ? val : undefined;
