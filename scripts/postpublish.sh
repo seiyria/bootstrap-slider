58140b1a08db55e9abc5d8705bf447b4d374c5d7
Rohit Kalkur <rohit.kalkur@gmail.com>
2015-08-09 12:32:29 -0400

    refactoring postpublish script

--- scripts/postpublish.sh
+++ scripts/postpublish.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# Version bump (patch)
-grunt bump-only:patch
-# Generate new dist
-grunt prod
-# Commit new release tag
-grunt bump-commit
+currentBranch=$(git rev-parse --abbrev-ref HEAD)
 
-# Push commits/tags to master branch on remote 'origin'
-git push origin master && git push --tags origin master
+if [ currentBranch = "master" ]
+then
+	echo "Running postpublish script"
 
-# Push new source code to gh-pages branch
+	# Version bump (patch)
+	grunt bump-only:patch
+	# Generate new dist
+	grunt prod
+	# Commit new release tag
+	grunt bump-commit
+
+	# Push commits/tags to master branch on remote 'origin'
+	git push origin master && git push --tags origin master
+
+	# Push new source code to gh-pages branch
+	
+else
+	echo "Cannot run postpublish script on branch that is not 'master'"
+fi
