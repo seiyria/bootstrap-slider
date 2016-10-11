92a67b60f2140c60312e1d4406dd98c677d5c2f6
George Viaud <george@mediaforce.com>
2016-10-11 14:07:24 -0700

    unwound use of keyword 'let'
    Keyword let is not understood by many older browsers.
    Using var in parent block instead.

--- src/js/bootstrap-slider.js
+++ src/js/bootstrap-slider.js
@@ -451,7 +451,8 @@ var windowIsDefined = (typeof window === "object");
 				/* Create highlight range elements */
 				this.rangeHighlightElements = [];
 				if (Array.isArray(this.options.rangeHighlights) && this.options.rangeHighlights.length > 0) {
-					for (let j = 0; j < this.options.rangeHighlights.length; j++) {
+					var j;
+					for (j = 0; j < this.options.rangeHighlights.length; j++) {
 
 						var rangeHighlightElement = document.createElement("div");
 						rangeHighlightElement.className = "slider-rangeHighlight slider-selection";
