3741bd41b2713e95ae7e0629543966c69cc8fb90
Django Doucet <djunglelabs@gmail.com>
2016-11-09 23:29:36 -0500

    Describe Aria-valuetext Tests

--- test/specs/AriaValueTextFormatterSpec.js
+++ test/specs/AriaValueTextFormatterSpec.js
@@ -1,27 +1,30 @@
-it("Sets the aria-valuetext to 'formatter' value", function() {
-  var textValArray = new Array('Monday','Wednesday','Friday');
-  var tooltipFormatter = function(value) {
-    var arrActiveValue = value;
-    return 'Current value: ' + textValArray[arrActiveValue-1];
-  };
-  
-  //Formatter is used
-  var testSlider = $("#accessibilitySliderA").slider({
-    formatter : tooltipFormatter
-  });
-  testSlider.slider('setValue', 2);
-  
-  var tooltipMessage = $("#accessibilitySliderA").siblings(".slider").children(".min-slider-handle").attr("aria-valuetext");
-  var expectedMessage = tooltipFormatter(2);
-  expect(tooltipMessage).toBe(expectedMessage);
- 
-});
-it("Does not use aria-valuetext if 'formatter' is not used", function() {
-	  
-  //Formatter is not used  
-  var testSliderB = $("#accessibilitySliderB").slider({});
-  testSliderB.slider('setValue', 2);
-
-  var ariaValueText = $("#accessibilitySliderB").siblings(".slider").children(".min-slider-handle").attr("aria-valuetext");
-  expect(ariaValueText).not.toBeDefined();
+describe("Aria-valuetext Tests", function() {
+    it("Sets the aria-valuetext to 'formatter' value", function() {
+      var textValArray = new Array('Monday','Wednesday','Friday');
+      var tooltipFormatter = function(value) {
+        var arrActiveValue = value;
+        return 'Current value: ' + textValArray[arrActiveValue-1];
+      };
+      
+      //Formatter is used
+      var testSlider = new Slider("#accessibilitySliderA", {
+      	id: "accessibilitySliderA",
+        formatter : tooltipFormatter
+      });
+      testSlider.slider('setValue', 2);
+      
+      var tooltipMessage = $("#accessibilitySliderA").siblings(".slider").children(".min-slider-handle").attr("aria-valuetext");
+      var expectedMessage = tooltipFormatter(2);
+      expect(tooltipMessage).toBe(expectedMessage);
+     
+    });
+    it("Does not use aria-valuetext if 'formatter' is not used", function() {
+    	  
+      //Formatter is not used  
+      var testSliderB = $("#accessibilitySliderB").slider({});
+      testSliderB.slider('setValue', 2);
+    
+      var ariaValueText = $("#accessibilitySliderB").siblings(".slider").children(".min-slider-handle").attr("aria-valuetext");
+      expect(ariaValueText).not.toBeDefined();
+    });
 });
