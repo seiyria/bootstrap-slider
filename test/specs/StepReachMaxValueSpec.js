6dd5b85fb509396a3d31b1d664fa8b5b5c3bf75b
Thomas Haitzer <thomas.haitzer@cashpresso.com>
2017-07-06 16:09:41 +0200

    fix indentation

--- test/specs/StepReachMaxValueSpec.js
+++ test/specs/StepReachMaxValueSpec.js
@@ -1,67 +1,69 @@
 describe("TickMaxValueNotATickBehavior", function() {
-	var SLIDER_ID = "testSlider1";
-	var slider;
-	var options;
+  var SLIDER_ID = "testSlider1";
+  var slider;
+  var options;
 
-	describe('max value should be reached', function() {
-		beforeEach(function() {
-			options = {
-				min: 39,
-				max: 1310,
-				step: 5,
-        scale:"logarithmic",
-				value: 44
-			};
+  describe('max value should be reached', function() {
+    beforeEach(function() {
+      options = {
+        min: 39,
+        max: 1310,
+        step: 5,
+        scale: "logarithmic",
+        value: 44
+      };
 
-			slider = new Slider(document.getElementById(SLIDER_ID), options);
-		});
+      slider = new Slider(document.getElementById(SLIDER_ID), options);
+    });
 
-		it("Value should contain max value when slider is moved to outer right position", function() {
+    it("Value should contain max value when slider is moved to outer right position", function() {
 
-			var sliderLeft = slider.sliderElem.offsetLeft;
+      var sliderLeft = slider.sliderElem.offsetLeft;
       var offsetY = slider.sliderElem.offsetTop;
-			var offsetX = sliderLeft + slider.sliderElem.clientWidth+10;
+      var offsetX = sliderLeft + slider.sliderElem.clientWidth + 10;
 
       var expectedValue = slider.options.max;
-			var mouseEvent = getMouseDownEvent(offsetX, offsetY);
+      var mouseEvent = getMouseDownEvent(offsetX, offsetY);
 
-			slider.mousedown(mouseEvent);
-			slider.mouseup();
+      slider.mousedown(mouseEvent);
+      slider.mouseup();
 
-			expect(slider.getValue()).toBe(expectedValue);
-		});
-	});
+      expect(slider.getValue()).toBe(expectedValue);
+    });
+  });
 
 
 
-	afterEach(function() { slider.destroy(); });
+  afterEach(function() {
+    slider.destroy();
+  });
 
-	// helper functions
+  // helper functions
 
 
   function getMouseDownEvent(offsetXToClick, offsetYToClick) {
-		var args = [
-			'mousedown', // type
-			true, // canBubble
-			true, // cancelable
-			document, // view,
-			0, // detail
-			0, // screenX
-			0, // screenY
-			offsetXToClick, // clientX
-			offsetYToClick, // clientY,
-			false, // ctrlKey
-			false, // altKey
-			false, // shiftKey
-			false, // metaKey,
-			0, // button
-			null // relatedTarget
-		];
+    var args = [
+      'mousedown', // type
+      true, // canBubble
+      true, // cancelable
+      document, // view,
+      0, // detail
+      0, // screenX
+      0, // screenY
+      offsetXToClick, // clientX
+      offsetYToClick, // clientY,
+      false, // ctrlKey
+      false, // altKey
+      false, // shiftKey
+      false, // metaKey,
+      0, // button
+      null // relatedTarget
+    ];
 
-		var event = document.createEvent('MouseEvents');
-		event.initMouseEvent.apply(event, args);
+    var event = document.createEvent('MouseEvents');
+    event.initMouseEvent.apply(event, args);
 
-		return event;
-	}
+    return event;
+  }
 
 });
