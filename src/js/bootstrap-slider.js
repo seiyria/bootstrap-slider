62bae31cc09f3924196cc0c47c866753cb033acd
Joachim Fischer <j.fischer@abberior-instruments.com>
2017-07-10 22:57:08 +0200

    changed if syntax to pass CI test

--- src/js/bootstrap-slider.js
+++ src/js/bootstrap-slider.js
@@ -902,7 +902,9 @@ const windowIsDefined = (typeof window === "object");
 				if (!val) {
 					val = 0;
 				}
-                if (!originalEvt) originalEvt = {};
+                if (!originalEvt) {
+				    originalEvt = {};
+                }
 
 				var oldValue = this.getValue();
 				this._state.value = this._validateInputValue(val);
@@ -1783,7 +1785,9 @@ const windowIsDefined = (typeof window === "object");
 			},
 			_trigger: function(evt, val, originalEvt) {
 				val = (val || val === 0) ? val : undefined;
-                if (!originalEvt) originalEvt = {};
+                if (!originalEvt) {
+                    originalEvt = {};
+                }
 
 				var callbackFnArray = this.eventToCallbackMap[evt];
 				if(callbackFnArray && callbackFnArray.length) {
