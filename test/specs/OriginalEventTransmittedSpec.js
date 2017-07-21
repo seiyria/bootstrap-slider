bdcab8ba84f6c4daac0cfb54fcd4bbdee6df2ea9
Joachim Fischer <j.fischer@abberior-instruments.com>
2017-07-21 21:45:54 +0200

    ...

--- test/specs/OriginalEventTransmittedSpec.js
+++ test/specs/OriginalEventTransmittedSpec.js
@@ -10,7 +10,7 @@
 */
 describe("'originalEvent' slider tests", function() {
   var testSlider, sliderId = "origEvtTestSlider",
-      $slider, originalEvent_passed
+      $slider, originalEvent_passed,
 	  sliderOptions = {id: sliderId, value: [0, 10]}; // for the sake of testing, always display the tooltip
 
   describe("When a slide event is triggered", function() {
