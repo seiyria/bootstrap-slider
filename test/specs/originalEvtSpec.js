82523a25c252dcd5280b99b714abf66c87cff023
Joachim Fischer <j.fischer@abberior-instruments.com>
2017-07-24 09:57:52 +0200

    ...

--- test/specs/originalEvtSpec.js
+++ test/specs/originalEvtSpec.js
@@ -110,4 +110,27 @@ describe("Original Event trasmittance test", function () {
         return event;
     }
 
+    // MouseEvent constructor  polyfill
+    (function (window) {
+        try {
+            new MouseEvent('test');
+            return false; // No need to polyfill
+        } catch (e) {
+            // Need to polyfill - fall through
+        }
+
+        // Polyfills DOM4 MouseEvent
+
+        var MouseEvent = function (eventType, params) {
+            params = params || { bubbles: false, cancelable: false };
+            var mouseEvent = document.createEvent('MouseEvent');
+            mouseEvent.initMouseEvent(eventType, params.bubbles, params.cancelable, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+
+            return mouseEvent;
+        }
+
+        MouseEvent.prototype = Event.prototype;
+
+        window.MouseEvent = MouseEvent;
+    })(window);
 });
