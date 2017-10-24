13dc3ee699460883b93f3cdd1eddaa1b521013d8
Sebastiaan ten Pas <sebastiaan.tenpas@patchman.co>
2017-10-24 08:49:19 +0200

    Make use of node_modules for bootstrap in gh-pages

--- scripts/update-gh-pages.sh
+++ scripts/update-gh-pages.sh
@@ -8,17 +8,19 @@ echo "..."
 grunt build-gh-pages
 # Create temporary copy of index file
 cp index.html index-temp.html
+cp node_modules/bootstrap/dist/css/bootstrap.min.css temp/bootstrap.min.css
 # Checkout to `gh-pages` branch
 git checkout -B gh-pages origin/gh-pages
 git pull -r origin gh-pages
 # Replace current files with temporary files
 mv index-temp.html index.html
 mv temp/bootstrap-slider.css css/bootstrap-slider.css
+mv temp/bootstrap.min.css css/bootstrap.min.css
 mv temp/bootstrap-slider.js js/bootstrap-slider.js
 # Remove /temp directory
 rm -rf temp
 # Stage new files for commit
-git add index.html css/bootstrap-slider.css js/bootstrap-slider.js
+git add index.html css/bootstrap-slider.css js/bootstrap-slider.js css/bootstrap.min.css
 # Create commit with new files
 git commit -m "updates"
 # Push new source code to gh-pages branch
