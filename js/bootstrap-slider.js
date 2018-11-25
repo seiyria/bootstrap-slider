05f8b8c7b6b8d06f248354e65ac529cf3abfa73b
Jeffrey Espiritu <jespiritu@myseneca.ca>
2018-11-24 20:51:45 -0500

    updates

--- js/bootstrap-slider.js
+++ js/bootstrap-slider.js
@@ -381,7 +381,15 @@ var windowIsDefined = (typeof window === "undefined" ? "undefined" : _typeof(win
 
 			// Check options.rtl
 			if (this.options.rtl === 'auto') {
-				this.options.rtl = window.getComputedStyle(this.element).direction === 'rtl';
+				var computedStyle = window.getComputedStyle(this.element);
+				if (computedStyle != null) {
+					this.options.rtl = computedStyle.direction === 'rtl';
+				} else {
+					// Fix for Firefox bug in versions less than 62:
+					// https://bugzilla.mozilla.org/show_bug.cgi?id=548397
+					// https://bugzilla.mozilla.org/show_bug.cgi?id=1467722
+					this.options.rtl = this.element.style.direction === 'rtl';
+				}
 			}
 
 			/*
