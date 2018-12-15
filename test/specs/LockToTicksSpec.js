e8b36f2afae1ec6ae3251d4d77379ac292a607fa
Jeffrey Espiritu <jespiritu@myseneca.ca>
2018-12-15 16:18:14 -0500

    Add `lock_to_ticks` unit tests
    Test for cases when `ticks[]` is not defined and when `setAttribute()`
    may be called during runtime.

--- test/specs/LockToTicksSpec.js
+++ test/specs/LockToTicksSpec.js
@@ -13,7 +13,77 @@ describe("Slider with lock_to_ticks: true tests", function() {
 
 	var testSlider;
 
-	it("Should snap to closest tick value", function() {
+	it("Should set `options.lock_to_ticks` to false when `ticks[]` is not set", function() {
+		testSlider = $('#testSlider1').slider({
+			lock_to_ticks: true
+		});
+
+		var lockAttr = testSlider.slider('getAttribute', 'lock_to_ticks');
+		expect(lockAttr).toBe(false);
+	});
+
+	it("Should set the slider value when `ticks[]` is not set", function() {
+		testSlider = $('#testSlider1').slider({
+			min: 0,
+			max: 10,
+			value: 1,
+			lock_to_ticks: true
+		});
+
+		testSlider.slider('setValue', 5);
+
+		var value = testSlider.slider('getValue');
+		expect(value).toBe(5);
+	});
+
+	it("Should return a value (after calling `setAttribute()`)", function() {
+		testSlider = $('#testSlider1').slider({
+			min: 0,
+			max: 10,
+			value: 1,
+			lock_to_ticks: true
+		});
+
+		// Try to lock to ticks when there are no ticks[]
+		testSlider.slider('setAttribute', 'lock_to_ticks', true);
+		testSlider.slider('setValue', 5);
+
+		// Without proper checking, getValue() is going to return NaN in this case
+		var value = testSlider.slider('getValue');
+		expect(isNaN(value)).toBe(false);
+	});
+
+	it("Should snap to closest tick value (single)", function() {
+		testSlider = $('#testSlider1').slider({
+			value: 1,
+			ticks: [1, 10, 50, 200, 500, 1000, 5000, 10000],
+			lock_to_ticks: true
+		});
+
+		// Set a value that is not a tick
+		testSlider.slider('setValue', 102);
+
+		var value = testSlider.slider('getValue');
+		expect(value).toBe(50);
+	});
+
+	it("Should snap to closest tick value (single, vertical)", function() {
+		testSlider = $('#testSlider1').slider({
+			value: 1,
+			ticks: [1, 10, 50, 200, 500, 1000, 5000, 10000],
+			lock_to_ticks: true,
+			orientation: 'vertical'
+		});
+
+		//selecting values that are not inside tickes
+		testSlider.slider('setValue', 102);
+
+		//getting the actual values. They should be the closest values from ticks (which are 1 and 50 on this case)
+		var value = testSlider.slider('getValue');
+		expect(value).toBe(50);
+	});
+
+	it("Should snap to closest tick value (range)", function() {
 		testSlider = $("#testSlider1").slider({
 			value: [1, 10000],
 			ticks: [1, 10, 50, 200, 500, 1000, 5000, 10000],
@@ -29,7 +99,7 @@ describe("Slider with lock_to_ticks: true tests", function() {
 		expect(values[1]).toBe(50);
 	});
 
-	it("Should snap to closest tick value (vertical orientation)", function() {
+	it("Should snap to closest tick value (range, vertical)", function() {
 		testSlider = $("#testSlider1").slider({
 			value: [1, 10000],
 			ticks: [1, 10, 50, 200, 500, 1000, 5000, 10000],
@@ -47,9 +117,9 @@ describe("Slider with lock_to_ticks: true tests", function() {
 	});
 
 	afterEach(function() {
-    if(testSlider) {
-      testSlider.slider('destroy');
-      testSlider = null;
-    }
+		if(testSlider) {
+			testSlider.slider('destroy');
+			testSlider = null;
+		}
 	});
 });
