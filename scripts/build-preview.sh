13bd094b8d3e26bd3b546ec955bdfe8210729bff
seiyria <kyle@seiyria.com>
2019-01-29 13:29:38 -0600

    mkdir before trying to use folders

--- scripts/build-preview.sh
+++ scripts/build-preview.sh
@@ -4,8 +4,12 @@
 
 # log
 # Generate index.html and /temp assets for GH Pages branch
+npm i
 grunt build
 grunt build-gh-pages
+
+mkdir css
+mkdir js
 
 cp node_modules/bootstrap/dist/css/bootstrap.min.css css/bootstrap.min.css
 mv temp/bootstrap-slider.css css/bootstrap-slider.css
